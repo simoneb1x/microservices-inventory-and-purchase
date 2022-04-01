021886207f247120c61d2aa9b6e2222258353a63
simoneb1x <moneb1x@gmail.com>
2022-04-01 16:08:32 +0200

    created two components for the frontend, Product and Wrapper. the fetch to get the products actually works

---
+++ inventory-frontend/src/components/Product.js
@@ -0,0 +1,52 @@
+import React, { Component, useEffect, useState }  from 'react';
+import { Wrapper } from "./Wrapper";
+
+export const Products = () => {
+
+    const [products, setProducts] = useState([]);
+
+    // Getting the products
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('http://localhost:8000/products');
+            const content = await response.json();
+            setProducts(content);
+        })();
+    }, []);
+
+  return (
+    <Wrapper>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Price</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+              {products.map(product => {
+                  return (
+                    <tr key={product.id}>
+                      <td>{product.id}</td>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>{product.quantity}</td>
+                      <td>
+                          <a href="#" className='btn btn-sm btn-outline-secondary'>
+                              Delete
+                          </a>
+                      </td>
+                    </tr>
+                  );
+              })}
+            
+          </tbody>
+        </table>
+      </div>
+    </Wrapper>
+  );
+};
