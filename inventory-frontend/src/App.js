ed5f710a48c212d08aa2fb9e94aed5babb359e5f
simoneb1x <moneb1x@gmail.com>
2022-04-01 16:28:07 +0200

    create product functionality from frontend now works fine

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component }  from 'react';
 import { Products } from "./components/Product";
+import { ProductsCreate } from './components/ProductsCreate';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Products />} />
+      <Route path="/create" element={<ProductsCreate />} />
     </Routes>
   </BrowserRouter>;
 }
--- inventory-frontend/src/components/Product.js
+++ inventory-frontend/src/components/Product.js
@@ -1,21 +1,26 @@
-import React, { Component, useEffect, useState }  from 'react';
+import React, { Component, useEffect, useState } from "react";
 import { Wrapper } from "./Wrapper";
+import { Link } from "react-router-dom";
 
 export const Products = () => {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([]);
-
-    // Getting the products
-    useEffect(() => {
-        (async () => {
-            const response = await fetch('http://localhost:8000/products');
-            const content = await response.json();
-            setProducts(content);
-        })();
-    }, []);
+  // Getting the products
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:8000/products");
+      const content = await response.json();
+      setProducts(content);
+    })();
+  }, []);
 
   return (
     <Wrapper>
+      <div className="pt-3 pb-2 mb-3 border-bottom">
+        <Link to={"create"} className="btn btn-sm btn-outline-secondary">
+          Add
+        </Link>
+      </div>
       <div className="table-responsive">
         <table className="table table-striped table-sm">
           <thead>
@@ -28,22 +33,21 @@ export const Products = () => {
             </tr>
           </thead>
           <tbody>
-              {products.map(product => {
-                  return (
-                    <tr key={product.id}>
-                      <td>{product.id}</td>
-                      <td>{product.name}</td>
-                      <td>{product.price}</td>
-                      <td>{product.quantity}</td>
-                      <td>
-                          <a href="#" className='btn btn-sm btn-outline-secondary'>
-                              Delete
-                          </a>
-                      </td>
-                    </tr>
-                  );
-              })}
-            
+            {products.map((product) => {
+              return (
+                <tr key={product.id}>
+                  <td>{product.id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.quantity}</td>
+                  <td>
+                    <a href="#" className="btn btn-sm btn-outline-secondary">
+                      Delete
+                    </a>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
