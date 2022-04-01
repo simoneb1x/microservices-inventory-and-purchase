fbdc6c69fc6edadad275efd747e580995ae19cf6
simoneb1x <moneb1x@gmail.com>
2022-04-01 10:55:33 +0200

    order class works! order_completed function defined

--- payment/main.py
+++ payment/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 from starlette.requests import Request
+import requests
+import time
 
 # FastAPI app
 app = FastAPI()
@@ -35,5 +37,28 @@ class Order(HashModel):
         database = redis
 
 @app.post('/orders')
-async def create(request: Request):
+async def create(request: Request): # this will send just ID and quantity. from the ID it will get the product
     body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id = body['id'],
+        price = product['price'],
+        fee = 0.2 * product['price'],
+        total = 1.2 * product['price'],
+        quantity = body['quantity'],
+        status = 'pending'
+    )
+
+    order.save()
+
+    order_completed(order)
+
+    return order
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
